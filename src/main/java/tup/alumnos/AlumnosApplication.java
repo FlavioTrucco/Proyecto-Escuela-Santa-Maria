8c43f1358c495a1c31825b757a448b92db8f40b4
FlavioTrucco <105309945+FlavioTrucco@users.noreply.github.com>
2022-10-14 14:46:43 -0300

    Add files via upload

---
+++ src/main/java/tup/alumnos/AlumnosApplication.java
@@ -0,0 +1,13 @@
+package tup.alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumnosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/tup/alumnos/models/User.java
@@ -0,0 +1,42 @@
+package tup.alumnos.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String curso;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
