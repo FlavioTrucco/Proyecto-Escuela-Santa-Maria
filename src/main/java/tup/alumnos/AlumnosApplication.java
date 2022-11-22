755acad0d7de1ec36accac096157443ba478b755
FlavioTrucco <flaviotrucco66@gmail.com>
2022-11-22 19:57:21 -0300

    Se Agregan Comentarios

--- src/main/java/tup/alumnos/AlumnosApplication.java
+++ src/main/java/tup/alumnos/AlumnosApplication.java
@@ -1,12 +1,15 @@
 package tup.alumnos;
 
+// TODO Es la clase principal de la aplicacion, la genera el asistente de configuracion
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication 
 
 /*Anotación que le dice a Spring que el proyecto va a ser una aplicación de SpringBoot, además activa 3
 herramientas de configuración @EnableConfiguration,  @ComponentScan y @SpringBootConfiguration. */
-@SpringBootApplication
+
 public class AlumnosApplication {
 
 	public static void main(String[] args) {
